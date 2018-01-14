8b42077753ae7e5d5e4aa7a6e290451e598e282a
Stanley Fok <stanleyfok@gmail.com>
2018-01-14 13:24:52 +0800

    remove dotenv

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
 
-// load dotenv if file exists
-/*
-if (fs.existsSync('./.env')) {
-  dotenv.config();
-}
-*/
-
 module.exports = {
   setupFiles: [
     '<rootDir>/jest.setup.js',
